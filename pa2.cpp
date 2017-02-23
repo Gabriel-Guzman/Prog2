9411b80d6db8b7b0fbe272250f5949d3c3e75934
Gabriel Guzman <gguzman@lin113-18.cise.ufl.edu>
2017-02-23 13:08:42 -0500

    Adding some notes.

---
+++ pa2.cpp
@@ -0,0 +1,16 @@
+#include<stdio>
+
+class Node{
+	private:
+		typename data;
+		Node * next;
+	public:
+		//Methods and stuff
+}
+
+class LinkedList {
+	private:	
+		Node * node;
+	public:
+		//methods and stuff
+}
